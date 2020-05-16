16c7978534ef6f30d140a2c0d3ecabf399ff8bbd
Donovan Lo <donovanlo1174@gmail.com>
2020-05-16 15:44:57 +0000

    Add test script to access api using axios

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,9 +1,28 @@
-var settings = {
-  "url": "https://eservices.mas.gov.sg/api/action/datastore/search.json?resource_id=95932927-c8bc-4e7a-b484-68a66a24edfe&limit=100&fields=end_of_day,usd_sgd&between%5Bend_of_day%5D=2020-01-01,2020-03-31&sort=end_of_day asc",
-  "method": "GET",
-  "timeout": 0,
-};
+// Test Script for jQuery
+// var settings = {
+//   "url": "https://eservices.mas.gov.sg/api/action/datastore/search.json?resource_id=95932927-c8bc-4e7a-b484-68a66a24edfe&limit=100&fields=end_of_day,usd_sgd&between%5Bend_of_day%5D=2020-01-01,2020-03-31&sort=end_of_day%20asc",
+//   "method": "GET",
+//   "timeout": 0,
+// };
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
+
+// Test Script for axios
+let apiURL='https://eservices.mas.gov.sg/api/action/datastore/search.json'
+let apiKey = 'RV76S9OYG1OF0XEU'
+$(function(){
+  $('#search-btn').click(function(){
+    axios.get(apiURL, {
+      params: {
+        'resource_id':'95932927-c8bc-4e7a-b484-68a66a24edfe',
+        'limit':'100',
+        'between[end_of_day]':'2020-01-01,2020-03-31',
+        'sort':'end_of_day asc'
+      }
+    }).then(function(response){
+      $("#result").text(response.data);
+    })
+  })
+})
